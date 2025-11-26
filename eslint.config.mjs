390e9944c5c57eba43768934cae1d0d914cf41b7
dali012 <dali.jerbi97@gmail.com>
2025-11-26 16:04:06 +0100

    feat: Configure `eslint-plugin-check-file` for naming conventions and add a new test component.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,19 +4,36 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettierPlugin from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
+import checkFilePlugin from 'eslint-plugin-check-file';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
 
   {
-    plugins: { prettier: prettierPlugin },
+    plugins: { prettier: prettierPlugin, 'check-file': checkFilePlugin },
     rules: {
       'prettier/prettier': 'error',
       'quotes': ['error', 'single'],
       'semi': ['error'],
       'prefer-arrow-callback': ['error'],
       'prefer-template': ['error'],
+      "check-file/no-index": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE"
+        },
+        {
+          "ignoreMiddleExtensions": true
+        }
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**": "NEXT_JS_APP_ROUTER_CASE"
+        }
+      ]
     },
   },
   prettierConfig,
