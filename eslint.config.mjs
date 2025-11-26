67ced651a812a8bc1887de6ac2ee48ed6e31a1fd
dali012 <dali.jerbi97@gmail.com>
2025-11-26 15:35:36 +0100

    config prettier + eslint + import sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,32 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+/* eslint-disable prettier/prettier */
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      'prettier/prettier': 'error',
+      'quotes': ['error', 'single'],
+      'semi': ['error'],
+      'prefer-arrow-callback': ['error'],
+      'prefer-template': ['error'],
+    },
+  },
+  prettierConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
